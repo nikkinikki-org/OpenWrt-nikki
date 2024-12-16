c1e07ca20a0807a8c7f95c7a2923818c7b54a8a6
Joseph Mory <morytyann@outlook.com>
2024-12-16 06:57:18 +0000

    refactor LuCI app
    Signed-off-by: Joseph Mory <morytyann@outlook.com>

--- luci-app-mihomo/htdocs/luci-static/resources/view/mihomo/app.js
+++ luci-app-mihomo/htdocs/luci-static/resources/view/mihomo/app.js
@@ -2,22 +2,94 @@
 'require form';
 'require view';
 'require uci';
+'require poll';
 'require tools.mihomo as mihomo';
+
+function renderStatus(running) {
+    return updateStatus(E('input', { id: 'core_status', style: 'border: unset; font-style: italic; font-weight: bold;', readonly: '' }), running);
+}
+
+function updateStatus(element, running) {
+    if (element) {
+        element.style.color = running ? 'green' : 'red';
+        element.value = running ? _('Running') : _('Not Running');
+    }
+    return element;
+}
 
 return view.extend({
     load: function () {
         return Promise.all([
             uci.load('mihomo'),
+            mihomo.appVersion(),
+            mihomo.coreVersion(),
+            mihomo.status(),
             mihomo.listProfiles(),
         ]);
     },
     render: function (data) {
         const subscriptions = uci.sections('mihomo', 'subscription');
-        const profiles = data[1];
+        const appVersion = data[1];
+        const coreVersion = data[2];
+        const running = data[3];
+        const profiles = data[4];
 
         let m, s, o;
 
-        m = new form.Map('mihomo');
+        m = new form.Map('mihomo', _('MihomoTProxy'), `${_('Transparent Proxy with Mihomo on OpenWrt.')} <a href="https://github.com/morytyann/OpenWrt-mihomo/wiki" target="_blank">${_('How To Use')}</a>`);
+
+        s = m.section(form.NamedSection, 'status', 'status', _('Status'));
+
+        o = s.option(form.Value, '_app_version', _('App Version'));
+        o.readonly = true;
+        o.load = function () {
+            return appVersion.trim();
+        };
+        o.write = function () { };
+
+        o = s.option(form.Value, '_core_version', _('Core Version'));
+        o.readonly = true;
+        o.load = function () {
+            return coreVersion.trim();
+        };
+        o.write = function () { };
+
+        o = s.option(form.DummyValue, '_core_status', _('Core Status'));
+        o.cfgvalue = function () {
+            return renderStatus(running);
+        };
+        poll.add(function () {
+            return L.resolveDefault(mihomo.status()).then(function (running) {
+                updateStatus(document.getElementById('core_status'), running);
+            });
+        });
+
+        o = s.option(form.Button, 'reload', '-');
+        o.inputstyle = 'action';
+        o.inputtitle = _('Reload Service');
+        o.onclick = function () {
+            return mihomo.reload();
+        };
+
+        o = s.option(form.Button, 'restart', '-');
+        o.inputstyle = 'negative';
+        o.inputtitle = _('Restart Service');
+        o.onclick = function () {
+            return mihomo.restart();
+        };
+
+        o = s.option(form.Button, 'update_dashboard', '-');
+        o.inputstyle = 'positive';
+        o.inputtitle = _('Update Dashboard');
+        o.onclick = function () {
+            return mihomo.callMihomoAPI('POST', '/upgrade/ui');
+        };
+
+        o = s.option(form.Button, 'open_dashboard', '-');
+        o.inputtitle = _('Open Dashboard');
+        o.onclick = function () {
+            return mihomo.openDashboard();
+        };
 
         s = m.section(form.NamedSection, 'config', 'config', _('App Config'));
 
--- luci-app-mihomo/htdocs/luci-static/resources/view/mihomo/log.js
+++ luci-app-mihomo/htdocs/luci-static/resources/view/mihomo/log.js
@@ -2,7 +2,6 @@
 'require form';
 'require view';
 'require uci';
-'require fs';
 'require poll';
 'require tools.mihomo as mihomo';
 
@@ -22,76 +21,17 @@ return view.extend({
     load: function () {
         return Promise.all([
             uci.load('mihomo'),
-            mihomo.appVersion(),
-            mihomo.coreVersion(),
-            mihomo.status(),
             mihomo.getAppLog(),
             mihomo.getCoreLog()
         ]);
     },
     render: function (data) {
-        const appVersion = data[1];
-        const coreVersion = data[2];
-        const running = data[3];
-        const appLog = data[4];
-        const coreLog = data[5];
+        const appLog = data[1];
+        const coreLog = data[2];
 
         let m, s, o;
 
-        m = new form.Map('mihomo', _('MihomoTProxy'), `${_('Transparent Proxy with Mihomo on OpenWrt.')} <a href="https://github.com/morytyann/OpenWrt-mihomo/wiki" target="_blank">${_('How To Use')}</a>`);
-
-        s = m.section(form.NamedSection, 'status', 'status', _('Status'));
-
-        o = s.option(form.Value, '_app_version', _('App Version'));
-        o.readonly = true;
-        o.load = function () {
-            return appVersion.trim();
-        };
-        o.write = function () { };
-
-        o = s.option(form.Value, '_core_version', _('Core Version'));
-        o.readonly = true;
-        o.load = function () {
-            return coreVersion.trim();
-        };
-        o.write = function () { };
-
-        o = s.option(form.DummyValue, '_core_status', _('Core Status'));
-        o.cfgvalue = function () {
-            return renderStatus(running);
-        };
-        poll.add(function () {
-            return L.resolveDefault(mihomo.status()).then(function (running) {
-                updateStatus(document.getElementById('core_status'), running);
-            });
-        });
-
-        o = s.option(form.Button, 'reload', '-');
-        o.inputstyle = 'action';
-        o.inputtitle = _('Reload Service');
-        o.onclick = function () {
-            return mihomo.reload();
-        };
-
-        o = s.option(form.Button, 'restart', '-');
-        o.inputstyle = 'negative';
-        o.inputtitle = _('Restart Service');
-        o.onclick = function () {
-            return mihomo.restart();
-        };
-
-        o = s.option(form.Button, 'update_dashboard', '-');
-        o.inputstyle = 'positive';
-        o.inputtitle = _('Update Dashboard');
-        o.onclick = function () {
-            return mihomo.callMihomoAPI('POST', '/upgrade/ui');
-        };
-
-        o = s.option(form.Button, 'open_dashboard', '-');
-        o.inputtitle = _('Open Dashboard');
-        o.onclick = function () {
-            return mihomo.openDashboard();
-        };
+        m = new form.Map('mihomo');
 
         s = m.section(form.NamedSection, 'log', 'log', _('Log'));
 
